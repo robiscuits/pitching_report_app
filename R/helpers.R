9b5466ece0d76706bab75e8018120966d47d46af
Robert George <60416883+robiscuits@users.noreply.github.com>
2025-12-02 16:06:49 -0600

    helpers commit

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,19 @@
 library(tidyverse)
 library(ggplot2)
 
-raw_data = read.csv("data/CWS ML Analyst Dataset.csv")
+#Load Data
+load_data = function(path){
+  raw_data = read.csv(path)
+  features = raw_data %>%
+    mutate(
+      is_strike = description %in% c("called_strike", "foul", "swinging_strike", "in_play"),
+      is_swing  = description %in% c("foul", "swinging_strike", "in_play"),
+      is_whiff  = description == "swinging_strike",
+      is_chase  = is_swing & (plate_x < -0.83 | plate_x > 0.83 | plate_z < 1.5 | plate_z > 3.5)
+    )
+}
+
+# Percentage helper
+pct = function(n, d) {
+  ifelse(d == 0, 0, round(100 * n / d, 1))
+}
--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+source("R/helpers.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
